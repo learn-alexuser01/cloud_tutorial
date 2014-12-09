5560669b502405d2fcc8f7b601dd7daa5ac7dca7
soutchay <chungvus@gmail.com>
2014-12-09 11:10:09 -0800

    add preserve-3d to cloudBase for layers

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function generate() {
         } 
     }
     //Create j amount of clouds
-    for( var j = 0; j < 20; j++ ) {
+    for( var j = 0; j < 5; j++ ) {
     	//Push cloud created from createCloud() function to objects array
         objects.push( createCloud() );
     }
@@ -76,14 +76,32 @@ function createCloud() {
 	var random_y = 256 - ( Math.random() * 512 );
 	var random_z = 256 - ( Math.random() * 512 );
 
+
     var t = 'translateX( ' + random_x + 'px ) translateY( ' + random_y + 'px ) translateZ( ' + random_z + 'px )';
     div.style.webkitTransform = t;
     var colorR = Math.floor(Math.random()*255);
     var colorG = Math.floor(Math.random()*255);
-    var colorB = Math.floor(Math.random()*255)
+    var colorB = Math.floor(Math.random()*255);
     div.style.backgroundColor = "rgb(" + colorR+"," + colorG+ ","+ colorB + ")";
     world.appendChild( div );
-    
+
+//add layers to cloud
+    for( var j = 0; j < 10 + Math.round( Math.random() * 10 ); j++ ) {
+        var cloud = document.createElement( 'div' );
+        cloud.className = 'cloudLayer';
+        
+        var random_z = 100 - ( Math.random() * 200 ); 
+		var random_a = Math.random() * 360;
+		var random_s = .25 + Math.random();      
+		// var random_y = 256 - ( Math.random() * 512 );
+        var t = 'translateX( ' + random_x + 'px ) translateY( ' + random_y + 'px ) translateZ( ' + random_z + 'px ) rotateZ( ' + random_a + 'deg ) scale( ' + random_s + ' )';
+        cloud.style.webkitTransform = t;
+        
+        div.appendChild( cloud );
+        layers.push( cloud );
+    }
+ 
+
     return div;
 }
 
@@ -97,76 +115,3 @@ function onContainerMouseWheel( event ) {
 }
 
 
-
-	
-	// var layers = [],
-	// 	objects = [],
-		
-	// 	world = document.getElementById( 'world' ),
-	// 	viewport = document.getElementById( 'viewport' ),
-		
-	// 	d = 0,
-	// 	p = 400,
-	// 	worldXAngle = 0,
-	// 	worldYAngle = 0;
-	
-	// viewport.style.webkitPerspective = p;
-	// viewport.style.MozPerspective = p;
-	// viewport.style.oPerspective = p;
-	
-	// generate();
-	
-	// function createCloud() {
-	
-	// 	var div = document.createElement( 'div'  );
-	// 	div.className = 'cloudBase';
-	// 	var x = 256 - ( Math.random() * 512 );
-	// 	var y = 256 - ( Math.random() * 512 );
-	// 	var z = 256 - ( Math.random() * 512 );
-	// 	var t = 'translateX( ' + x + 'px ) translateY( ' + y + 'px ) translateZ( ' + z + 'px )';
-	// 	div.style.webkitTransform = t;
-	// 	div.style.MozTransform = t;
-	// 	div.style.oTransform = t;
-	// 	world.appendChild( div );
-		
-	// 	return div;
-	// }
-	
-	// window.addEventListener( 'mousewheel', onContainerMouseWheel );
-	// window.addEventListener( 'DOMMouseScroll', onContainerMouseWheel ); 
-
-	// window.addEventListener( 'mousemove', function( e ) {
-	// 	worldYAngle = -( .5 - ( e.clientX / window.innerWidth ) ) * 180;
-	// 	worldXAngle = ( .5 - ( e.clientY / window.innerHeight ) ) * 180;
-	// 	updateView();
-	// } );
-	
-	// function generate() {
-	// 	objects = [];
-	// 	if ( world.hasChildNodes() ) {
-	// 		while ( world.childNodes.length >= 1 ) {
-	// 			world.removeChild( world.firstChild );       
-	// 		} 
-	// 	}
-	// 	for( var j = 0; j < 5; j++ ) {
-	// 		objects.push( createCloud() );
-	// 	}
-	// }
-	
-	// function updateView() {
-	// 	var t = 'translateZ( ' + d + 'px ) rotateX( ' + worldXAngle + 'deg) rotateY( ' + worldYAngle + 'deg)';
-	// 	world.style.webkitTransform = t;
-	// 	world.style.MozTransform = t;
-	// 	world.style.oTransform = t;
-	// }
-	
-	// function onContainerMouseWheel( event ) {
-			
-	// 	event = event ? event : window.event;
-	// 	d = d - ( event.detail ? event.detail * -5 : event.wheelDelta / 8 );
-	// 	updateView();
-		
-	// }
-	
-
-	
