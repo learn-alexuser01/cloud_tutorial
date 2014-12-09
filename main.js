915abf65831cc06f6251714fca8d25cd5964e281
soutchay <chungvus@gmail.com>
2014-12-09 11:48:45 -0800

    add 3D effect function update

--- main.js
+++ main.js
@@ -90,11 +90,22 @@ function createCloud() {
         var cloud = document.createElement( 'div' );
         cloud.className = 'cloudLayer';
         
-        var random_z = 100 - ( Math.random() * 200 ); 
-		var random_a = Math.random() * 360;
-		var random_s = .25 + Math.random();      
+       	// var random_x = 256 - ( Math.random() * 512 );
 		// var random_y = 256 - ( Math.random() * 512 );
-        var t = 'translateX( ' + random_x + 'px ) translateY( ' + random_y + 'px ) translateZ( ' + random_z + 'px ) rotateZ( ' + random_a + 'deg ) scale( ' + random_s + ' )';
+        var random_z = 100 - ( Math.random() * 200 ); 
+		var random_d = Math.random() * 360;
+		var random_s = .25 + Math.random();
+		// add an object for data to allow for the 3D effect
+		cloud.data = { 
+			x: random_x,
+			y: random_y,
+			z: random_z,
+			a: random_d,
+			s: random_s,
+			speed: .1 * Math.random()
+		};		     
+		// var random_y = 256 - ( Math.random() * 512 );
+        var t = 'translateX( ' + random_x + 'px ) translateY( ' + random_y + 'px ) translateZ( ' + random_z + 'px ) rotateZ( ' + random_d + 'deg ) scale( ' + random_s + ' )';
         cloud.style.webkitTransform = t;
         
         div.appendChild( cloud );
@@ -105,7 +116,7 @@ function createCloud() {
     return div;
 }
 
-//function to determine z translation
+//function to determine z translation viewpoint
 function onContainerMouseWheel( event ) {
 		
 	event = event ? event : window.event;
@@ -115,3 +126,26 @@ function onContainerMouseWheel( event ) {
 }
 
 
+/*
+    Iterate layers[], update the rotation and apply the
+    inverse transformation currently applied to the world.
+    Notice the order in which rotations are applied.
+*/
+function update (){
+        
+    for( var j = 0; j < layers.length; j++ ) {
+        var layer = layers[ j ];
+        layer.data.a += layer.data.speed;
+        var t = 'translateX( ' + layer.data.x + 'px ) \
+            translateY( ' + layer.data.y + 'px ) \
+            translateZ( ' + layer.data.z + 'px ) \
+            rotateY( ' + ( - worldYAngle ) + 'deg ) \
+            rotateX( ' + ( - worldXAngle ) + 'deg ) \
+            scale( ' + layer.data.s + ')';
+        layer.style.webkitTransform = t;
+    }
+    
+    requestAnimationFrame( update );
+    
+}
+update();
